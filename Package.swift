31c9bfc8949dc3ce07d18f9c249c5f494f8df485
luvgarg24 <139758917+luvgarg24@users.noreply.github.com>
2026-09-13 00:50:30 +0530

    Build NUKE macOS MVP

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "NUKE",
+    platforms: [.macOS(.v14)],
+    products: [.executable(name: "NUKE", targets: ["NUKE"])],
+    targets: [.executableTarget(name: "NUKE", path: "Sources/NUKE")]
+)
